100efbb9cab20f67bdf3f8e04b58110720bf2fe6
malibu-desktop <swallow.smiley@outlook.jp>
2024-02-04 00:31:39 +0900

    make Desktop ver

---
+++ desktop/sa2pull.bat
@@ -0,0 +1,3 @@
+cd /d D:\steam_env\steamapps\common\Sonic Adventure 2\resource\gd_PC\SAVEDATA
+git pull
+pause
---
+++ desktop/sa2push.bat
@@ -0,0 +1,5 @@
+cd /d D:\steam_env\steamapps\common\Sonic Adventure 2\resource\gd_PC\SAVEDATA
+git add .
+git commit -m "%date% %time%"
+git push origin main
+pause
