6372f2a3648b6fc81f6dd141d56406c64b0acdac
malibuorange <65383804+malibuorange@users.noreply.github.com>
2024-04-08 00:33:54 +0900

    Update sa2push.bat

--- desktop/sa2push.bat
+++ desktop/sa2push.bat
@@ -2,4 +2,6 @@ cd /d D:\steam_env\steamapps\common\Sonic Adventure 2\resource\gd_PC\SAVEDATA
 git add .
 git commit -m "%date% %time%"
 git push origin main
+del /Q SONIC2B__ALF
+del /Q SONIC2B__S01
 pause
